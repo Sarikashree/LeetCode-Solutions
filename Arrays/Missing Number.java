0fd84aa73c42007ae6e9f6786f1a0140dcb1e752
Sarikashree <shreesarika74@gmail.com>
2026-03-29 21:07:36 +0530

    Create Missing Number.java
    Find the expected sum of numbers from 0 to n
    Find the actual sum of the array
    
    The missing number =
    
    expected sum - actual sum

---
+++ Arrays/Missing Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int actual=0;
+        int expected=n*(n+1)/2;
+        for(int num:nums)
+        {
+            actual += num;
+        }
+        int missing=expected-actual;
+        return missing;
+    }
+}
